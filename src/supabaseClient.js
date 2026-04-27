53344a399adc41f8dbac463fede95d8b29b18245
Mega-jota <107134337+Mega-jota@users.noreply.github.com>
2026-04-27 14:03:19 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,7 +7,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co'       // ← Reemplaza con tu Project URL
-const SUPABASE_ANON_KEY = 'TU-ANON-KEY-AQUI'                 // ← Reemplaza con tu anon/public key
+const SUPABASE_URL = 'https://mkkwbytjateachszpebp.supabase.co'       // ← Reemplaza con tu Project URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ra3dieXRqYXRlYWNoc3pwZWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNTA2NjksImV4cCI6MjA5MjcyNjY2OX0.bWefkGHXLK8pHJA23T4Es5ngkVlrQGuY-PfJ1rzCHbg'                 // ← Reemplaza con tu anon/public key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
